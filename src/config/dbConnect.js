cc60bf96de68a97c8c2968502b7f2818cd396389
ShaneKanishka98 <166781874+ShaneKanishka98@users.noreply.github.com>
2025-07-04 16:22:30 +0400

    db connection complete

---
+++ src/config/dbConnect.js
@@ -0,0 +1,13 @@
+const { Sequelize } = require("sequelize");
+
+require("dotenv").config();
+
+// Option 3: Passing parameters separately (other dialects)
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect:
+    "postgres" /* one of 'mysql' | 'postgres' | 'sqlite' | 'mariadb' | 'mssql' | 'db2' | 'snowflake' | 'oracle' */,
+  // logging: false, // Disable logging; default: console.log
+});
+
+module.exports = sequelize;
