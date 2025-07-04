40aba723db38dacdcf1f0431345064f3c73a3535
ShaneKanishka98 <166781874+ShaneKanishka98@users.noreply.github.com>
2025-07-04 14:31:22 +0400

    simple express server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-// root file (serveer)
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// App Listener
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  try {
+    console.log("Server is running on port 3001");
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+});
